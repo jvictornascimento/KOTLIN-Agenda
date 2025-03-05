d73790c714178cee3cbff1dd0e5ca2f52e61d043
victojoao <victojoao789@gmail.com>
2025-03-05 10:47:28 -0300

    Project agenda created

---
+++ src/main/kotlin/com/jvictornascimento/agenda/AgendaApplication.kt
@@ -0,0 +1,11 @@
+package com.jvictornascimento.agenda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgendaApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgendaApplication>(*args)
+}
---
+++ src/test/kotlin/com/jvictornascimento/agenda/AgendaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jvictornascimento.agenda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
