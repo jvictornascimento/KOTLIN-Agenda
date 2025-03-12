a1c1f190c89dcee43ae4523b1814a6be6081d641
victojoao <victojoao789@gmail.com>
2025-03-12 17:43:24 -0300

    Integration test: method findById

--- src/test/kotlin/com/jvictornascimento/agenda/controllers/PersonControllerTest.kt
+++ src/test/kotlin/com/jvictornascimento/agenda/controllers/PersonControllerTest.kt
@@ -1,23 +1,31 @@
 package com.jvictornascimento.agenda.controllers
 
 import com.jvictornascimento.agenda.dtos.CompletePersonDTO
+import com.jvictornascimento.agenda.dtos.PersonCreateDTO
+import com.jvictornascimento.agenda.mapper.toCompletePersonDTO
+import com.jvictornascimento.agenda.mapper.toPersonModel
 import com.jvictornascimento.agenda.models.AddressModel
 import com.jvictornascimento.agenda.models.ContactModel
 import com.jvictornascimento.agenda.services.PersonService
+import com.jvictornascimento.agenda.services.exceptions.IdNotFoundException
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PersonControllerTest {
     @Autowired
     private lateinit var webTestClient: WebTestClient
+    @MockitoBean
+    private lateinit var personService:PersonService
 
     val address = AddressModel(
         1L,
@@ -29,7 +37,7 @@ class PersonControllerTest {
     val contact = ContactModel(
         1L,
         19996686866)
-    val person = CompletePersonDTO(
+    val person = PersonCreateDTO(
         1L,
         "João Victor Santos do Nascimento",
         27,
@@ -42,11 +50,43 @@ class PersonControllerTest {
     }
 
     @Test
-    fun findById() {
+    fun testFindByIdSuccess() {
+        `when`(personService.getById(1L)).thenReturn(person.toPersonModel().toCompletePersonDTO())
+        webTestClient
+            .post()
+            .uri("/person")
+            .bodyValue(person)
+            .exchange()
+            .expectStatus().isCreated
+        webTestClient
+            .get()
+            .uri("/person/1")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CompletePersonDTO::class.java)
+            .consumeWith { response ->
+                val responseBody = response.responseBody
+                assert(responseBody!!.name == person.name)
+                assert(responseBody.age == person.age)
+                assert(responseBody.email == person.email)
+                assert(responseBody.addresses == person.addresses)
+                assert(responseBody.contacts == person.contacts)
+            }
+    }
+    @Test
+    fun testFindByIdFailure() {
+        `when`(personService.getById(2L)).thenThrow(IdNotFoundException(2L))
+
+        webTestClient
+            .get()
+            .uri("/person/2")
+            .exchange()
+            .expectStatus().isNotFound
     }
 
     @Test
     fun testCreatePersonSuccess() {
+        `when`(personService.createPerson(person)).thenReturn(person.toPersonModel().toCompletePersonDTO())
         webTestClient
             .post()
             .uri("/person")
